6abd9fb9085621a5b10708e530f6cfc89bcb767b
José <joigno@gmail.com>
2026-04-21 21:35:29 +0200

    Bump to v0.3.2 - fix POSIX compilation for manylinux

--- include/logosdb/logosdb.h
+++ include/logosdb/logosdb.h
@@ -6,8 +6,8 @@
 
 #define LOGOSDB_VERSION_MAJOR 0
 #define LOGOSDB_VERSION_MINOR 3
-#define LOGOSDB_VERSION_PATCH 1
-#define LOGOSDB_VERSION_STRING "0.3.1"
+#define LOGOSDB_VERSION_PATCH 2
+#define LOGOSDB_VERSION_STRING "0.3.2"
 
 #ifdef __cplusplus
 extern "C" {
